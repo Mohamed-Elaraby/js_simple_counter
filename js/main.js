baf6ba430b76e4164127cc477cb0e95b30100537
Mohamed <mido.b333@gmail.com>
2022-07-11 16:43:45 +0200

    edit code 1

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 $(document).ready(function (){
     let counter = $('#counter');
-    let start = 5000;
-    counter.html(start);
+    let start = 3;
+    change_Counter_value(start)
 
     let interval = setInterval(function (){
         if (counter.text() > 1)
         {
-            counter.html(counter.text()-1);
+            change_Counter_value(counter.text()-1)
         }else
         {
             clearInterval(interval)
-            counter.html('Welcome');
+            change_Counter_value('welcome')
         }
     }, 1000)
 
+    function change_Counter_value (value)
+    {
+        counter.html(value);
+    }
 })
